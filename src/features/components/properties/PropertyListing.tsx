c2ea22a4145703b3adac5c199ac3d492695c7c7c
hamzakamran <hamzakwebdev@gmail.com>
2024-09-04 10:56:07 -0500

    Property images

--- src/features/components/properties/PropertyListing.tsx
+++ src/features/components/properties/PropertyListing.tsx
@@ -10,7 +10,7 @@ const PropertyListing = ({ property }: Props) => {
 	return (
 		<section className="column gap-3">
 			{property.images.length > 0 && (
-				<HeaderGallery images={property.images.slice(0, 5)} />
+				<HeaderGallery images={property.images} />
 			)}
 			{property.units.length > 0 && <PropertyInfo property={property} />}
 		</section>
